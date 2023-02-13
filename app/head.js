03cc6f0ea0586ab62f8cb8d9eb0f210c69177ca4
Emerson Carvalho <emeron@quiknode.io>
2023-02-12 21:44:58 -0300

    using app dir

---
+++ app/head.js
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>Emerson Broga Dev</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
---
+++ app/layout.js
@@ -0,0 +1,8 @@
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <head />
+      <body className="bg-slate-900 text-slate-100 min-h-screen">{children}</body>
+    </html>
+  );
+}
